a927cb3e39b25e6f1e5a0c48248168508b10f420
Agatha Turyahikayo <agatha@Agathas-MacBook-Pro.local>
2018-09-08 16:05:59 -0400

    added another info slide

--- app/app/src/main/java/melanotix/melanotix/InstructionsActivity.java
+++ app/app/src/main/java/melanotix/melanotix/InstructionsActivity.java
@@ -22,6 +22,8 @@ public class InstructionsActivity extends AhoyOnboarderActivity {
 
         AhoyOnboarderCard ahoyOnboarderCard2 = new AhoyOnboarderCard("Communicate", "Communicate this info with your doctor", R.drawable.doctor);
 
+        AhoyOnboarderCard ahoyOnboarderCard3 = new AhoyOnboarderCard("How this works", "This app uses an algorithm to check if your mole is displaying signs of melanoma, a dangerous form of skin cancer", R.drawable.rsz_question_mark);
+
         ahoyOnboarderCard1.setTitleTextSize(25);
         ahoyOnboarderCard1.setDescriptionTextSize(20);
         ahoyOnboarderCard1.setDescriptionColor(R.color.colorPrimary);
@@ -32,9 +34,15 @@ public class InstructionsActivity extends AhoyOnboarderActivity {
         ahoyOnboarderCard2.setDescriptionColor(R.color.colorPrimaryDark);
         ahoyOnboarderCard2.setTitleColor(R.color.colorPrimaryDark);
 
+        ahoyOnboarderCard3.setTitleTextSize(25);
+        ahoyOnboarderCard3.setDescriptionTextSize(20);
+        ahoyOnboarderCard2.setDescriptionColor(R.color.colorPrimaryDark);
+        ahoyOnboarderCard2.setTitleColor(R.color.colorPrimaryDark);
+
 
         ahoyOnboarderCard1.setBackgroundColor(R.color.blue_transparent);
         ahoyOnboarderCard2.setBackgroundColor(R.color.blue_transparent);
+        ahoyOnboarderCard3.setBackgroundColor(R.color.blue_transparent);
 
 
         setImageBackground(R.drawable.panoramic);
@@ -43,6 +51,7 @@ public class InstructionsActivity extends AhoyOnboarderActivity {
         List<AhoyOnboarderCard> pages = new ArrayList<>();
         pages.add(ahoyOnboarderCard1);
         pages.add(ahoyOnboarderCard2);
+        pages.add(ahoyOnboarderCard3);
 
 
         for (AhoyOnboarderCard page : pages) {
