583e9fd3b416557aab7e624a5bf8477d3f4c07b1
Agatha Turyahikayo <agatha@Agathas-MacBook-Pro.local>
2018-09-08 09:28:59 -0400

    added animated transition

--- app/app/src/main/java/melanotix/melanotix/MenuActivity.java
+++ app/app/src/main/java/melanotix/melanotix/MenuActivity.java
@@ -1,7 +1,13 @@
 package melanotix.melanotix;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.fujiyuu75.sequent.Sequent;
+
+import spencerstudios.com.bungeelib.Bungee;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +15,9 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.layout);
+        Sequent.origin(layout).start();
+
+
     }
 }
