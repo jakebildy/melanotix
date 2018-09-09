f6b1930a3874864d7b09ee7636b62a08e01208ce
jakebildy <jbildy@uwo.ca>
2018-09-08 20:55:06 -0400

    tweaks to website

--- app/app/src/main/assets/sendphoto.js
+++ app/app/src/main/assets/sendphoto.js
@@ -21,27 +21,33 @@ var downloadTimer = setInterval(function(){
 
         // Sending and receiving data in JSON format using POST method
 
-        // Make the REST API call.
-        $.ajax({
-            url: "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/1b2c0a21-2177-4299-aa3f-fe218235cdc4/url",
+        var xhr = new XMLHttpRequest();
+        var url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/1b2c0a21-2177-4299-aa3f-fe218235cdc4/url";
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
 
-            // Request headers.
-            beforeSend: function(xhrObj){
-                xhrObj.setRequestHeader("Content-Type","application/json");
-                xhrObj.setRequestHeader(
-                    "Prediction-Key", "e468e9927724421ea6995c7293a13788");
-            },
+        xhr.setRequestHeader("Prediction-Key", "e468e9927724421ea6995c7293a13788");
 
-            type: "POST",
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var json = JSON.parse(xhr.responseText);
 
-            // Request body.
-            data: '{"url": ' + '"' + "https://www.medicalnewstoday.com/content/images/hero/079/79115/79115_256.jpg" + '"}',
-        })
+                alert(json)
 
-            .done(function(data) {
-                // Show formatted JSON on webpage.
-                alert(JSON.stringify(data));
-            })
+            }
+            else
+            {
+                var json = JSON.parse(xhr.responseText);
+
+                alert(xhr.status)
+            }
+        };
+
+
+        alert(img.url);
+        var data = JSON.stringify('{"Url": "https://www.medicalnewstoday.com/content/images/hero/079/79115/79115_256.jpg"}');
+
+        xhr.send(data);
     }
 
 },1000);
